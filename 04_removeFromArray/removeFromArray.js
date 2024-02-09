9bf0c67ed62e5b79360aa82d158b6fb16d8145a6
owaym <owaym123@gmail.com>
2024-02-09 13:20:03 +0530

    Solved and Closed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(anArray, ...args) {
+    const newArray = [];
+    
+    anArray.forEach((item) => {
+        if(!args.includes(item))
+        {
+            newArray.push(item);
+        }
+        
+    });
+    return newArray;
 };
 
 // Do not edit below this line
